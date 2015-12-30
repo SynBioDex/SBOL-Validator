75f4d6c716b6f52033430037d12e4135260f323d
Zach Zundel <zachz@tang.ece.utah.edu>
2015-12-30 14:54:30 -0700

    Finished integration with Wordpress

--- sbol-validator.php
+++ sbol-validator.php
@@ -1,74 +1,80 @@
 <?php
-/*
-Plugin Name: SBOL Validator
-Plugin URI: http://async.ece.utah.edu/validator
-Description: This plugin validates SBOL files
-Version: 1.0
-Author: Zach Zundel
-*/
 
-function sbolvalidator_html_form() {
-	// Print the HTML for the form
-    if(empty($_POST["submit"])) {
-    	echo "<form action=\"" . esc_url( $_SERVER['REQUEST_URI'] ) . "\" method=\"post\" enctype=\"multipart/form-data\">";
-    	echo "Select SBOL file to validate:";
-    	echo "<input type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\">";
-    	echo "<br>";
-    	echo "Allow noncompliance: ";
-    	echo "<input type=\"checkbox\" name=\"noncompliant\" id=\"noncompliant\">";
-    	echo "<br>";
-    	echo "Allow incompleteness:";
-    	echo "<input type=\"checkbox\" name=\"incomplete\" id=\"incomplete\">";
-    	echo "<br>";
-    	echo "Enter a URI prefix for 1.0 to 2.0 conversion if desired:";
-    	echo "<input type=\"text\" name=\"prefix\" id=\"prefix\">";
-    	echo "<br>";
-    	echo "<input type=\"submit\" value=\"Upload for Validation\" name=\"submit\">";
-    	echo "</form>";
-    }
+//Print the form
+function sbolvalidator_html_form()
+{
+	echo "<form action=\"" . esc_url($_SERVER['REQUEST_URI']) . "\" method=\"post\" enctype=\"multipart/form-data\">";
+	echo "Select SBOL file to validate:";
+	echo "<input type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\">";
+	echo "<br>";
+	echo "Allow noncompliance: ";
+	echo "<input type=\"checkbox\" name=\"noncompliant\" id=\"noncompliant\">";
+	echo "<br>";
+	echo "Allow incompleteness:";
+	echo "<input type=\"checkbox\" name=\"incomplete\" id=\"incomplete\">";
+	echo "<br>";
+	echo "Enter a URI prefix for 1.0 to 2.0 conversion if desired:";
+	echo "<input type=\"text\" name=\"prefix\" id=\"prefix\">";
+	echo "<br>";
+	echo "<input type=\"submit\" value=\"Upload for Validation\" name=\"submit\">";
+	echo "</form>";
 }
 
-function validate() {
-    if(isset($_POST["submit"])) {
-        if ( ! function_exists( 'wp_handle_upload' ) ) {
-            require_once( ABSPATH . 'wp-admin/includes/file.php' );
-        }
-        $upload_overrides = array( 'test_form' => false );
-        $movefile = wp_handle_upload($_FILES["fileToUpload"]["tmp_name"], $upload_overrides);
-        var_dump($movefile);
-        if ($movefile && !isset($movefile['error'])) {
-            $pathparts = pathinfo($movefile['file']);
-            $command = "java -jar " . plugin_dir_path( __FILE__ ) . "libSBOLj-2.0.0-withDependencies.jar ";
-            $command = $command . $movefile['file'] . " ";
-            $command = $command . $pathparts['filename'] . "-validated." . $pathparts['extension'] . " ";
+//Upload file and run validation
+function validate()
+{
+	//Import Wordpress upload functions
+	if (!function_exists('wp_handle_upload')) {
+		require_once(ABSPATH . 'wp-admin/includes/file.php');
+	}
+	//Allow upload without form and allow upload of non-media filetypes
+	$upload_overrides = array('test_form' => false, 'mimes' => array(pathinfo($_FILES["fileToUpload"]["name"], PATHINFO_EXTENSION) => $_FILES["fileToUpload"]["type"]));
+	
+	//Add file to Wordpress uploads
+	$movefile = wp_handle_upload($_FILES["fileToUpload"], $upload_overrides);
 
-            if(isset($_POST["noncompliance"])) {
-                $command = $command . "-n ";
-            }
-
-            if(isset($_POST["incomplete"])) {
-                $command = $command . "-i ";
-            }
-            if($_POST["prefix"] != "") {
-                $command = $command . "-p " . escapeshellarg($_POST["prefix"]) . " ";
-            }
-    		$command = $command . '2>&1';
-            echo $command;
-            echo shell_exec($command);
-    		//$zip->open($target_zip, ZipArchive::CREATE);
-    		//$zip->addFile( $target_file . '.validated.sbol', $target_file  . '.rdf');
-    		echo "<br>";
-    		echo '<a href="' . site_url() . '/' . $target_file . '-validated.' . $extension . '">Converted and adjusted SBOL</a>';
-        } 
-        else {
-                echo "Sorry, there was an error uploading your file.";
-        }
-    }
+	//If upload is successful, run validation. Otherwise, explain failure.
+	if ($movefile && !isset($movefile['error'])) {
+		//Build shell command from form
+		$pathparts = pathinfo($movefile['file']);
+		$command = "java -jar " . plugin_dir_path(__FILE__) . "libSBOLj-2.0.0-withDependencies.jar ";
+		$command = $command . $movefile['file'] . " ";
+		$command = $command . "-o " . $pathparts['filename'] . "-validated." . $pathparts['extension'] . " ";
+		if (isset($_POST["noncompliance"])) {
+			$command = $command . "-n ";
+		}
+		if (isset($_POST["incomplete"])) {
+			$command = $command . "-i ";
+		}
+		if ($_POST["prefix"] != "") {
+			$command = $command . "-p " . escapeshellarg($_POST["prefix"]) . " ";
+		}
+		$command = $command . '2>&1';
+		
+		//Execute shell command
+		$result = shell_exec($command);
+		
+		//Print result, and if necessary, print link to valid SBOL
+		echo $result;
+		if (trim($result) == "Validation successful, no errors.") {
+			echo "<br>";
+			echo '<a href="' . $movefile["url"] . '">Converted and adjusted SBOL</a>';
+		}
+	} else {
+		echo "Sorry, there was an error uploading your file. <br><b>Wordpress says: " . $movefile['error'] . '</b>';
+	}
 }
-function sbolvalidator_shortcode(){
-	validate();
-	sbolvalidator_html_form();
+
+//If a POST request has been submitted, run validate method. Otherwise, display form.
+function sbolvalidator_shortcode()
+{
+	if (isset($_POST["submit"])) {
+		validate();
+	} else {
+		sbolvalidator_html_form();
+	}
 }
 
-add_shortcode( 'sbolvalidator', 'sbolvalidator_shortcode' );
+//Add shortcode for Wordpress use
+add_shortcode('sbolvalidator', 'sbolvalidator_shortcode');
 ?>
