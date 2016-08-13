5ea5898ba5957bb9c906838c64453b29ecb692b2
Zach Zundel <zach.zundel@ignilife.com>
2016-08-13 16:34:55 +0100

    Ready to integrate with validation methods

---
+++ src/validation-api/api.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class ValidationAPI(Resource):
+    def post(self):
+        pass
+
+api.add_resource(ValidationAPI, '/validate')
+
+if __name__ == '__main__':
+    app.run(debug=True)
