0a8e61f1a5a4658abff387c072a6db372ed786a9
Zach Zundel <16zzundel5@gmail.com>
2016-09-25 21:41:33 -0600

    Prepare for testing

--- src/validation-api/api.py
+++ src/validation-api/api.py
@@ -1,19 +1,19 @@
-from flask import Flask
-from flask_restful import Resource, Api
-
+from flask import Flask, request
+from validator import ValidationOptions, ValidationResult, ValidationRun
 
 app = Flask(__name__)
-api = Api(app)
 
-class ValidationAPI(Resource):
-    def post(self):
-        pass
+@app.route("/validate", methods=["POST"])
+def validate(self):
+    if not request.json
+    options = new ValidationOptions();
 
+    
+@app.route("/update", methods=["POST"])
 class UpdateAPI(Resource):
     def post(self):
         pass
 
-api.add_resource(ValidationAPI, '/validate')
 
 if __name__ == '__main__':
     app.run(debug=True)
