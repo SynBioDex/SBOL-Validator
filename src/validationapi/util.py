e92cf5905b77db8c022a3dd8586ae43e9dd50003
Zach Zundel <Zach Zundel>
2016-12-28 01:47:03 -0700

    Documentation and cleanup

--- src/validationapi/util.py
+++ src/validationapi/util.py
@@ -4,22 +4,25 @@ import os
 
 
 def do_validation(json):
+    """
+    Performs validation based on a json request
+    """
     options = ValidationOptions()
     options.build(json['options'])
 
     main_filename = os.path.join('work', str(uuid.uuid4()) + ".sbol")
-    with open(main_filename, 'a+') as f:
-        f.write(json["main_file"].encode('utf-8'))
-    
+    with open(main_filename, 'a+') as file:
+        file.write(json["main_file"].encode('utf-8'))
+
     if json['options']['test_equality']:
         diff_filename = os.path.join('work', str(uuid.uuid4()) + ".sbol")
 
-        with open(diff_filename, 'a+') as f:
-            f.write(json["diff_file"].encode("utf-8"))
+        with open(diff_filename, 'a+') as file:
+            file.write(json["diff_file"].encode("utf-8"))
 
         run = ValidationRun(options, main_filename, diff_filename)
     else:
         run = ValidationRun(options, main_filename)
-    
+
     result = run.execute()
     return result
