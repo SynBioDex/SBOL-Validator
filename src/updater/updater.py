4a35333b4d6a280293abde115b80d7e6237cf805
Zach Zundel <Zach Zundel>
2017-01-19 16:34:03 -0700

    added uwsgi reload instead of systemctl

--- src/updater/updater.py
+++ src/updater/updater.py
@@ -1,5 +1,6 @@
 from os.path import dirname, abspath
 import subprocess
+import uwsgi
 
 def update():
     project_dir = dirname(dirname(abspath(__file__)))
@@ -7,7 +8,4 @@ def update():
     if not subprocess.check_output(["/usr/bin/git pull"], shell=True, cwd=project_dir):
         return "Update unsuccessful. Check the logs."
 
-    if subprocess.call(["/bin/systemctl restart sbol-validator"], shell=True):
-        return "Update successful"
-    else:
-        return "Update unsuccessful. Check the logs."
+    uwsgi.reload()
