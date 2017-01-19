7854378e481e8b44eda49f0627224daf4c394107
Zach Zundel <Zach Zundel>
2017-01-19 13:26:09 -0700

    Updater shell

--- src/updater/updater.py
+++ src/updater/updater.py
@@ -2,4 +2,4 @@ import subprocess
 
 def update():
     command = "./update.sh"
-    subprocess.call(command, universal_newlines=True, stderr=subprocess.STDOUT)
+    subprocess.call(command, universal_newlines=True, stderr=subprocess.STDOUT, shell=True)
