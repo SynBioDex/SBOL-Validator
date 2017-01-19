1319abe7f53d8e54952d6aca5c5c65fb12eca46e
Zach Zundel <Zach Zundel>
2017-01-19 16:41:06 -0700

    Adjusted error messaging

--- src/validationapi/api.py
+++ src/validationapi/api.py
@@ -9,10 +9,7 @@ CORS(app)
 
 @app.route('/update/', strict_slashes=False)
 def update():
-    if run_update():
-        return "Update successful"
-    else:
-        return "Update unsuccessful. Please check error logs."
+    return run_update():
 
 
 @app.route("/validate/", methods=["POST"], strict_slashes=False)
