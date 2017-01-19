90fa5b53c8d41ea99052011e9b5afa0db8a30631
Zach Zundel <Zach Zundel>
2017-01-19 16:25:04 -0700

    Changed to remove all debug code

--- src/updater/updater.py
+++ src/updater/updater.py
@@ -3,14 +3,11 @@ import subprocess
 
 def update():
     project_dir = dirname(dirname(abspath(__file__)))
-    print(project_dir)
-    try:
-        print(subprocess.check_output(["/usr/bin/git pull"], shell=True, cwd=project_dir))
-    except subprocess.CalledProcessError as e:
-        print(e.output)
 
+    if not subprocess.check_output(["/usr/bin/git pull"], shell=True, cwd=project_dir):
+        return "Update unsuccessful. Check the logs."
 
-    if subprocess.call(["/bin/systemctl", "restart", "sbol-validator"], shell=True):
+    if subprocess.call(["/bin/systemctl restart sbol-validator"], shell=True):
         return "Update successful"
     else:
         return "Update unsuccessful. Check the logs."
