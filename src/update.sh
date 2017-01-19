e5311350d2ffdfd83e69bd1abf1d8f6e84cd87de
Zach Zundel <Zach Zundel>
2017-01-19 13:27:50 -0700

    Removed shell and added full path to execs

--- src/update.sh
+++ src/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 exec 3>&1 4>&2 > update.log 2>&1
 
-git pull
-systemctl restart sbol-validator
+/usr/bin/git pull
+/usr/bin/systemctl restart sbol-validator
 
 exec 1>&3 2>&4
--- src/updater/updater.py
+++ src/updater/updater.py
@@ -2,4 +2,4 @@ import subprocess
 
 def update():
     command = "./update.sh"
-    subprocess.call(command, universal_newlines=True, stderr=subprocess.STDOUT, shell=True)
+    subprocess.call(command, universal_newlines=True, stderr=subprocess.STDOUT)
