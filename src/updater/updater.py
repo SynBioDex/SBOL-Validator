850c7ccbeada6170abeb632d7069e03ca2d46ee9
root <root@shang.ece.utah.edu>
2017-02-02 02:52:48 -0700

    Fixed updater so that app can be run from command line

--- src/updater/updater.py
+++ src/updater/updater.py
@@ -1,6 +1,12 @@
 from os.path import dirname, abspath
 import subprocess
-import uwsgi
+
+try:
+    import uwsgi
+except ImportError:
+    print("Unable to import uWSGI application.")
+    print("This probably means that you're running the application manually.")
+    print("If you are, ignore this message.")
 
 def update():
     project_dir = dirname(dirname(abspath(__file__)))
