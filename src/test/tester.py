bb452da64f8cb24b35cbd9ca695778c681ebeab1
root <root@shang.ece.utah.edu>
2017-02-02 03:38:25 -0700

    Build test

--- src/test/tester.py
+++ src/test/tester.py
@@ -1,13 +1,1 @@
-import os
 import validationapi.api as api
-import unittest
-import tempfile
-
-class ValidationAPITestCase(unittest.TestCase):
-    def test_post(self):
-        rv = api.app.get('/validate')
-        print(rv)
-
-
-if __name__ == '__main__':
-    unittest.main()
