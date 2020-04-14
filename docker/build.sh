2952f99dc0842d1d49e218d97ec28d0278224eec
Zach Zundel <me@zachzundel.com>
2020-04-13 19:28:42 -0600

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
-docker build -t zachzundel/sbolvalidator:snapshot -f docker/validator.Dockerfile .
-docker build -t zachzundel/sbolconverter:snapshot -f docker/converter.Dockerfile .
+docker build -t myersresearchgroup/sbolvalidator:snapshot -f docker/validator.Dockerfile .
+docker build -t myersresearchgroup/sbolconverter:snapshot -f docker/converter.Dockerfile .
 
